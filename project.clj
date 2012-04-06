166c3c0b46c566f6bbd7858415be9651ac1d6223
Allen Johnson <allenj@simsol.com>
2012-04-06 14:29:31 -0400

    Bumped cheshire json lib version to 3.1.0

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [cheshire "2.0.2"]
+                 [cheshire "3.1.0"]
                  [com.rabbitmq/amqp-client "2.8.1"]])
